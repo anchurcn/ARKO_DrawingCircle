b4c129d8953f10e023072aef2c100411f5754852
Maciej Suchocki <M.Suchocki@stud.elka.pw.edu.pl>
2017-04-01 11:06:56 +0200

    All comments in english

--- circle.asm
+++ circle.asm
@@ -1,198 +1,198 @@
 .data
-	tekst: .asciiz "Podaj promien okregu do narysowania: "
+	tekst: .asciiz "Give the radius of the circle: "
 	nazwa_pliku: .asciiz "circle.bmp"
 	.align 2
 	header: .space 56
 .text
 
-#$s0 - szerokość/wysokość obrazka
-#$s1 - szerokość obrazka z nadmiarem (szerokość musi być wielkrotnością 4)
-#$s2 - adres bufora obrazka
-#$s3 - adres do początku pliku
-#$s4 - rozmiar całego obrazka
-#$s5 - promień okręgu
+#$s0 - width/height of image
+#$s1 - width of image with excess (multiplies of 4)
+#$s2 - address of the image buffer
+#$s3 - address to the beginning of the file
+#$s4 - the size of the entire image
+#$s5 - circle radius
 
-#Wyświetlam komunikat
+#Message display
 	li $v0, 4
 	la $a0, tekst
 	syscall
 	
-#Wczytuje promień okręgu
+#Loading the radius of the circle
 	li $v0, 5
 	syscall
 	move $s5, $v0
 	
-#Obliczam rozmiar obrazka 
-	add $s0, $v0, $v0	#Podwajam promień i zapisuje do $s0
-	addi $s0, $s0, 1	#Dodaje 1 do średnicy (środkowy piksel)
+#Calculating image size 
+	add $s0, $v0, $v0	#doubling radius and write to $ s0
+	addi $s0, $s0, 1	#adding 1 to diameter (middle pixel)
 	
-#Liczę nadmiar - zaokrąglam liczbę pikseli do 4 (w górę)
-	mul $s1, $s0, 3		#Ilość pikseli w szerokości obrazka (3 piksele na jeden bit)
-	subi $s1, $s1, 1	#Odejmuje 1
-	andi $s1, $s1, 0xfffffffc	#Maska bitowa - liczba całkowita powstała przez podzielenie $s1 przez 4 inaczej AND (*00) i $s1
-	addi $s1, $s1, 4	#Dodaje 4 aby zaokrąglic do 4 
+#Counting the excess - Rounding up the number of pixels to 4 (up)
+	mul $s1, $s0, 3		#number of pixels in image width (3 pixels per bit)
+	subi $s1, $s1, 1	#substract 1
+	andi $s1, $s1, 0xfffffffc	#bitmask - an integer is formed by dividing $s1 by 4 or in other words AND (* 00) and $s1
+	addi $s1, $s1, 4	#Adding 4 to round to 4 
 	
-#Wyświetlam z nadmiarem
+#Displaying with excess
 	li $v0, 1
 	add $a0, $s1, $zero
 	syscall
-#Przygotowuje nagłówek
-	la $t0, header		#Pobieram adres do bufora nagłówka, teraz w t0 jest 56 bitow
-	li $t1, 0x42		#Przygotowuje pierwszy znak (wynikajace z dokumentacji BMP)
-	sb $t1, 2($t0)		#Zapisuje
-	li $t1, 0x4D		#drugi znak 
+#Preparing header
+	la $t0, header		#getting an address to the header buffer, now in t0 is 56 bits
+	li $t1, 0x42		#reparing the first character (resulting from the BMP documentation)
+	sb $t1, 2($t0)		#write
+	li $t1, 0x4D		#secong char
 	sb $t1, 3($t0)		
 	
-#Alokacja pamięci na obrazek i zapis nagłówka
-	mul $t1, $s0, $s1	#piksele potrzebne na cały obraz - (wysokość) x (szerokość z nadmiarem)
-	add $s4, $t1, $zero	#Zapamiętaj wartość na później
+#Allocating memory to the image and writing the header
+	mul $t1, $s0, $s1	#pixels needed for the entire image - (height) x (width with excess)
+	add $s4, $t1, $zero	#remember value for later
 	
-	li $v0, 9		#Alokacja ilosci bajtow  (rozmiar)
+	li $v0, 9		#allocate byte size (size)
 	add $a0, $t1, $zero	
 	syscall
-	add $s2, $v0, $zero	#Zapiamiętaj adres do zaalokowanej pamięci w $s2
+	add $s2, $v0, $zero	#remember the address to the allocated memory in $ s2
 	
-	addi $t1, $t1, 54	#Rozmiar całego pliku czyli rozmiar obrazka i naglowek w $t1
-	sw $t1, 4($t0)		#Wpisz rozmiar całego pliku do nagłówka
+	addi $t1, $t1, 54	#size of the entire file is the size of the image and the headline in $ t1
+	sw $t1, 4($t0)		#enter the size of the entire file into the header
 	
-	li $t1, 54		#54 - offset danych (rozmiar nagłówka)
+	li $t1, 54		#54 - data offset (header size)
 	sw $t1, 12($t0)		
-	li $t1, 40		#40 - długość do końca nagłówka
+	li $t1, 40		#40 - length to end of header
 	sw $t1, 16($t0)		
-	add $t1, $s0, $zero	#Skopiuj szerokość/wysokość obrazka
-	sw $t1, 20($t0)		#szerokość
-	sw $t1, 24($t0)		#wysokość
-	li $t1, 1		#1 - ilość wartstw kolorów
+	add $t1, $s0, $zero	#copy image width / height
+	sw $t1, 20($t0)		#width
+	sw $t1, 24($t0)		#height
+	li $t1, 1		#1 - number of layers of colors
 	sw $t1, 28($t0)		
-	li $t1, 24		#24 - liczba bitów na piksel (3 kolory po 8 bitów każdy)
+	li $t1, 24		#24 - number of bits per pixel (3 colors of 8 bits each)
 	sb $t1, 30($t0)		
-#Otwórz plik do zapisu
-	li $v0, 13		#Otwieranie pliku
-	la $a0, nazwa_pliku	#Wczytanie adresu na nazwę pliku
-	li $a1, 1		#1 - bo plik do odczytu
-	li $a2, 0		#0 - flaga
+#Opening the file to write
+	li $v0, 13		#opening a file
+	la $a0, nazwa_pliku	#loading address into file name
+	li $a1, 1		#1 - file to read
+	li $a2, 0		#0 - flag
 	syscall
-	add $s3, $v0, $zero	#Zapamiętanie wskaźnika na plik w $s3 
-#Zapisz nagłówek
+	add $s3, $v0, $zero	#saving pointer to file in $ s3
+#Saving header
 	li $v0, 15		
-	add $a0, $s3, $zero	#Skopowianie wskaźnika na plik
-	la $a1, header+2	#Początek zapisywanych danych
-	li $a2, 54		#Ilość bitów do zapisania (rozmiar nagłówka)
+	add $a0, $s3, $zero	#copy pointer to file
+	la $a1, header+2	#start of recorded data
+	li $a2, 54		#number of bits to save (header size)
 	syscall
-#Algorytm Bresenhama
-	add $t0, $s5, $zero 	#Współrzędna X środka okręgu (X0) - równa promieniowi
-	move $t1, $t0		#Współrzędna Y środka okręgu (Y0) - równa promieniowi
+#Bresenham's algorithm
+	add $t0, $s5, $zero 	#coordinate X of circle center (X0) - equal to radius
+	move $t1, $t0		#coordinate Y of circle center (Y0) - equal to radius
 	li $t2, 5		#5
 	mul $t3, $s5, 4		#4*r
 	sub $t2, $t2, $t3	#d = 5 - 4*r	
-	li $t3, 0		#Współrzędna X - aktualna
-	move $t4, $t0		#Współrzędna Y - aktualna
+	li $t3, 0		#coordinate X - actual
+	move $t4, $t0		#coordinate Y - actual
 	mul $t5, $t4, -2 	#deltaA = -2*r
 	addi $t5, $t5, 5	#deltaA = 5 + deltaA = (5-2*r)
 	mul $t5, $t5, 4		#deltaA = deltaA * r = (5-2*r) * 4
 	li $t6, 12		#deltaB = 12
 
-petla:
-	bgt $t3, $t4, koniec	#Jeśli X>Y - cały okrąg narysowany
+loop:
+	bgt $t3, $t4, end	#If X>Y - circle is drawn
 	
-	#Ustawianie kolorów pikseli:
-	#Ustaw kolor piksela 1
+	#Setting pixel colors:
+	#Set the color of 1st pixel
 	sub $t7, $t0, $t3	# x0 - x
 	sub $t8, $t1, $t4	# y0 - y
-	mul $t7, $t7, 3		# *= 3 (bo po 3 piksele na jeden punkt)
-	mul $t8, $t8, $s1	# *= wielkość_wiersza (bo przesunięcie o ileśtam linii w dół)
-	add $t7, $t7, $t8	# Obecna pozycja piksela
-	add $t7, $t7, $s2	# Pozycja piksela względem początku pliku (dodaję do adresu początku pliku)
-	li $v0, 0xff		#Kolor na czarny
-	sb $v0, ($t7)		#Kolor niebieski
-	sb $v0, 1($t7)		#Kolor zielony
-	sb $v0, 2($t7)		#Kolor czerwony
-	#Ustaw kolor piksela 2
-	sub $t7, $t0, $t3 #x0 - x
-	add $t8, $t1, $t4 #y0 + y
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	mul $t7, $t7, 3		# *= 3 (3 pixels per point)
+	mul $t8, $t8, $s1	# *= size_of_line (moving down)
+	add $t7, $t7, $t8	#current pixel position
+	add $t7, $t7, $s2	#the position of the pixel relative to the beginning of the file (Adding to the beginning of the file)
+	li $v0, 0xff		#black
+	sb $v0, ($t7)		#blue
+	sb $v0, 1($t7)		#green
+	sb $v0, 2($t7)		#red
+	#Set the color of 2nd pixel
+	sub $t7, $t0, $t3	#x0 - x
+	add $t8, $t1, $t4	#y0 + y
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Ustaw kolor piksela 3
-	add $t7, $t0, $t3 #x0 + x
-	sub $t8, $t1, $t4 #y0 - y
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	#Set the color of 3rd pixel
+	add $t7, $t0, $t3	#x0 + x
+	sub $t8, $t1, $t4 	#y0 - y
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Ustaw kolor piksela 4
-	add $t7, $t0, $t3 #x0 + x
-	add $t8, $t1, $t4 #y0 + y
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	#Set the color of 4th pixel
+	add $t7, $t0, $t3 	#x0 + x
+	add $t8, $t1, $t4 	#y0 + y
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Ustaw kolor piksela 5
-	sub $t7, $t0, $t4 #x0 - y
-	sub $t8, $t1, $t3 #y0 - x
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	#Set the color of 5th pixel
+	sub $t7, $t0, $t4 	#x0 - y
+	sub $t8, $t1, $t3 	#y0 - x
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Ustaw kolor piksela 6
-	sub $t7, $t0, $t4 #x0 - y
-	add $t8, $t1, $t3 #y0 + x
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	#Set the color of 6th pixel
+	sub $t7, $t0, $t4 	#x0 - y
+	add $t8, $t1, $t3 	#y0 + x
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Ustaw kolor piksela 7
-	add $t7, $t0, $t4 #x0 + y
-	sub $t8, $t1, $t3 #y0 - x
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	#Set the color of 7th pixel
+	add $t7, $t0, $t4 	#x0 + y
+	sub $t8, $t1, $t3 	#y0 - x
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Ustaw kolor piksela 8
-	add $t7, $t0, $t4 #x0 + y
-	add $t8, $t1, $t3 #y0 + x
-	mul $t7, $t7, 3 # *= 3
-	mul $t8, $t8, $s1 # *= wielkość_wiersza
+	#Set the color of 8th pixel
+	add $t7, $t0, $t4 	#x0 + y
+	add $t8, $t1, $t3 	#y0 + x
+	mul $t7, $t7, 3 	# *= 3
+	mul $t8, $t8, $s1 	# *= size_of_the_line
 	add $t7, $t7, $t8
 	add $t7, $t7, $s2
 	li $v0, 0xff
 	sb $v0, ($t7)
 	sb $v0, 1($t7)
 	sb $v0, 2($t7)
-	#Kolory pikseli ustawione
+	#Pixel colors set
 	
-	bgtz $t2, d0		# d > 0   idź to d0
+	bgtz $t2, d0		# d > 0   
 	# d <= 0
 	add $t2, $t2, $t6	# d += deltaB
 	addi $t3, $t3, 1	# x += 1
 	addi $t5, $t5, 8	# deltaA += 2*4
 	addi $t6, $t6, 8	# deltaB += 2*4
-	b dalej 		
+	b move		
 d0:
 	# d > 0
 	add $t2, $t2, $t5	#d += deltaA
@@ -200,19 +200,19 @@ d0:
 	addi $t3, $t3, 1	#x += 1
 	addi $t5, $t5, 16	#deltaA += 4*4
 	addi $t6, $t6, 8	#deltaB += 2*4
-dalej:
-	b petla			#Skocz do następnego kroku
-koniec:
-#Zapisz resztę pliku
-	li $v0, 15		#zapis 
-	add $a0, $s3, $zero	#Skopiowanie wskaźnika na plik
-	add $a1, $s2, $zero	#Skopiowanie adresu bufora
-	add $a2, $s4, $zero	#Skopiowanie ilości pikseli obrazka
+move:
+	b loop			#jump to the next step
+end:
+#Saving the rest of the file
+	li $v0, 15		#save
+	add $a0, $s3, $zero	#copy pointer to file
+	add $a1, $s2, $zero	#copy address of buffer
+	add $a2, $s4, $zero	#copy number of image pixels
 	syscall
-#Zamknij plik
+#Closing
 	li $v0, 16		
-	add $a0, $s3, $zero	#Skopiowanie wskaźnika na plik
+	add $a0, $s3, $zero	#copy pointer to file
 	syscall
-#Zakończ program
+#Finish the program
 	li $v0, 10		
 	syscall
